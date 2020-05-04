0d65d37e8119a6b94489226add280d71d3fdf5b4
Patel Deep <63906505+pateldeep7799@users.noreply.github.com>
2020-05-04 14:57:00 +0530

    Add files via upload

---
+++ Diagonal Difference/Diagonal-difference-of-matrix.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[100][100],n,c=0,d=0,i,j,sum=0;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        cin>>a[i][j];
+    }
+    for(i=0;i<n;i++)
+    {
+        c =c+a[i][i];
+    }
+    for(i=0;i<n;i++)
+    {
+        d=d+a[i][n-1-i];
+    }
+    sum=abs(c-d);
+    cout<<sum;
+}
