ce2eb41f633d06f407680b2424396fd8a298cade
Patel Deep <63906505+pateldeep7799@users.noreply.github.com>
2020-05-04 15:46:17 +0530

    Add files via upload

---
+++ Birthday Cake Candle/Birthday Cake Candle.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int highest=-1, cnt=0;
+    for (int i=0;i<n;i++)
+    {
+        int h;
+        scanf("%d",&h);
+        if (h > highest)
+        {
+            highest = h;
+            cnt = 1;
+        }
+        else if (h == highest) // Note this has to be "else if" not "if" !!
+            cnt ++;
+    }
+    printf("%d\n", cnt);
+}
